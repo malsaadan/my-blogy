a198c6db1cbfdd1241f734d0e3aa73f3e4742a98
malsaadan <mmalsaadan@gmail.com>
2020-03-08 11:45:16 +0300

    Setup Database Connection

---
+++ config/db.js
@@ -0,0 +1,26 @@
+// Creating a base name for the MongoDB
+const mongooseBaseName = "blogy";
+
+// There are different versions of the db
+// blogy-development
+// blogy-test
+// blogy-staging
+// blogy-production
+// blogy
+
+// Create the MongoDB URI for Development and Test
+const database = {
+  development: `mongodb://localhost/${mongooseBaseName}-development`,
+  test: `mongodb://localhost/${mongooseBaseName}-test`
+};
+
+// Identify if development environment is Test or Development
+// Select DB based on wether a test file was executed before `server.js`
+const localDB = process.env.TESTENV ? database.test : database.development;
+
+// Environment variable MONGODB_URI will be available in 
+// Heroku production environment, otherwise use Test or Development DB
+const currentDB = process.env.MONGODB_URI || localDB;
+
+// Export the appropriate database based on the current environment
+module.exports = currentDB;
--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const mongoose = require('mongoose');
 // Require Route Files
 const indexRouter = require('./app/routes/index');
 
+// Require DB Configuration FIle
+const db = require('./config/db');
+
+// Establish Database Connection
+mongoose.connect(db, {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+    console.log('Connected to Mongo', db);
+});
+
 // Instantiate Express Application Object
 const app = express();
 
-// Define PORT for the API to run on into seperate variable (It changes on different env)
+// Define PORT for the API to run on into separate variable (It changes on different env)
 const port = process.env.PORT || 5000;  // process.env.PORT define port on the deployed env
 
 /***** Routes *****/

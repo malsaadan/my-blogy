a8fc1897ab0ee1343ef0ff9073eb7b173f1e471f
malsaadan <mmalsaadan@gmail.com>
2020-03-08 15:06:15 +0300

    Add Create Article Route

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -15,9 +15,9 @@ const router = express.Router();
  */
 
 router.get("/api/articles", (req, res) => {
-  Article.find()    // find() talks to the DB
+  Article.find() // find() talks to the DB
     // Return all Articles as an Array
-    .then((AllArticles) => {
+    .then(AllArticles => {
       res.status(200).json({ articles: AllArticles });
     })
     // Catch any error that might occur
@@ -28,35 +28,50 @@ router.get("/api/articles", (req, res) => {
     });
 });
 
+/*
+ * Action:       SHOW
+ * Method:       GET
+ * URI:          /api/article/9b9ceb63c8e8
+ * Description:  Get an Article by Article ID
+ */
 
-/* 
-    * Action:       SHOW
-    * Method:       GET
-    * URI:          /api/article/9b9ceb63c8e8
-    * Description:  Get an Article by Article ID
-*/
-
-/* 
-    * Action:       CREATE
-    * Method:       POST
-    * URI:          /api/articles
-    * Description:  Create a new Article
-*/
+/*
+ * Action:       CREATE
+ * Method:       POST
+ * URI:          /api/articles
+ * Description:  Create a new Article
+ */
+router.post("/api/articles", (req, res) => {
+  Article.create(req.body.article)
+    // On a successful `create` action, respond with 201
+    // HTTP status and the content of the new article
+    .then(newArticle => {
+      res.status(201).json({
+        article: newArticle
+      });
+    })
+    // Catch any errors that might occur
+    .catch(error => {
+      res.status(500).json({
+        error: error
+      });
+    });
+});
 
 // PUT updates whole document, PATCH doing some process then update
-/* 
-    * Action:       UPDATE
-    * Method:       PATCH
-    * URI:          /api/articles/9b9ceb63c8e8
-    * Description:  Update an Article by Article ID
-*/
+/*
+ * Action:       UPDATE
+ * Method:       PATCH
+ * URI:          /api/articles/9b9ceb63c8e8
+ * Description:  Update an Article by Article ID
+ */
 
-/* 
-    * Action:       DESTROY
-    * Method:       DELETE
-    * URI:          /api/articles/9b9ceb63c8e8
-    * Description:  Delete an Article by Article ID
-*/
+/*
+ * Action:       DESTROY
+ * Method:       DELETE
+ * URI:          /api/articles/9b9ceb63c8e8
+ * Description:  Delete an Article by Article ID
+ */
 
 // Export the Router so we can use it in the server.js file
 module.exports = router;
